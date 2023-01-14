3608a1e10e9a28df91db87b056147f8846399da5
IreneCreadora <nargiza.dancer@gmail.com>
2023-01-14 09:45:09 +0200

    add container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import { AiOutlineIdcard } from 'react-icons/ai';
+import { Container } from './ContactsPage.styled';
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <h1>
         <span role="img" aria-label="logo icon">
           <AiOutlineIdcard size="30px" color="#9f40ff" />
         </span>{' '}
         Task manager welcome page
       </h1>
-    </div>
+    </Container>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,14 @@
 import { Helmet } from 'react-helmet';
 import { LoginForm } from 'components/LoginForm/LoginForm';
+import { Container } from './ContactsPage.styled';
 
 export default function Login() {
   return (
-    <div>
+    <Container>
       <Helmet>
         <title>Login</title>
       </Helmet>
       <LoginForm />
-    </div>
+    </Container>
   );
 }
