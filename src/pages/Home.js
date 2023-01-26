4dd61629591b0ff8ab0bd82a931d13515f5454b6
IreneCreadora <nargiza.dancer@gmail.com>
2023-01-26 17:00:39 +0000

    add links for registation and login pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,35 @@
+import { Helmet } from 'react-helmet';
+import { useAuth } from 'hooks/useAuth';
 import { AiOutlineIdcard } from 'react-icons/ai';
-import { Container } from './ContactsPage.styled';
+import { WelcomeContainer, Header, LinkStyled } from './Home.styled';
 
 export default function Home() {
+  const { isLoggedIn } = useAuth();
   return (
-    <Container>
-      <h1>
-        <span role="img" aria-label="logo icon">
-          <AiOutlineIdcard size="30px" color="#9f40ff" />
-        </span>{' '}
-        Task manager welcome page
-      </h1>
-    </Container>
+    <WelcomeContainer>
+      <Helmet>
+        <title>Home</title>
+      </Helmet>
+      <span role="img" aria-label="logo icon">
+        <AiOutlineIdcard size="120px" color="#9f40ff" />
+      </span>
+      <Header>Welcome to Your Contacts Book</Header>
+      {!isLoggedIn && (
+        <p>
+          For you available{' '}
+          <LinkStyled to="/register"> registration </LinkStyled> or{' '}
+          <LinkStyled to="/login">login</LinkStyled> if you are already with us.
+          <br></br>
+          Enter your personal account, use our service and enjoy.
+        </p>
+      )}
+
+      {isLoggedIn && (
+        <p>
+          Whats up? to access your contacts, go to the{' '}
+          <LinkStyled to="/contacts">Phonebook</LinkStyled> !
+        </p>
+      )}
+    </WelcomeContainer>
   );
 }
